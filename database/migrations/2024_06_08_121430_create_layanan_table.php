29f9b2d749b084516acf094d69803dc7eaba5282
fikriiaabdullah <fikriiaabdul@gmail.com>
2024-06-08 21:20:53 +0700

    Update 2024_06_08_121430_create_layanan_table.php

--- database/migrations/2024_06_08_121430_create_layanan_table.php
+++ database/migrations/2024_06_08_121430_create_layanan_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('layanan', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100);
-            $table->int('price');
+            $table->integer('price');
             $table->timestamps();
         });
     }
