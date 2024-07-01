990d5b7812d779d868966cc61813d3fc65c54a6f
Muhammad Ryan Fikri Fakhrezi <113404595+Yanfiq@users.noreply.github.com>
2024-07-02 00:13:26 +0700

    wrong dir

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -126,7 +126,7 @@ class UserController extends Controller
         if ($request->hasFile('photo')) {
             $photo = $request->file('photo');
             // Ambil nama file dan simpan ke database
-            if ($user->photo_path != 'storage/userPictures/default_profile.png') {
+            if ($user->photo_path != 'storage/profilePictures/default_profile.png') {
                 Storage::disk('public')->delete(str_replace('storage/', '', $user->photo_path));
             }
             $fileName = time() . '_' . $photo->getClientOriginalName();
