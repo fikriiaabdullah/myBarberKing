eb420898e7dbed733ae3b7bb367ce7ad30365e98
fikriiaabdullah <fikriiaabdul@gmail.com>
2024-07-01 16:59:36 +0700

    lupa save

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('password');
             $table->string('phone', 100)->nullable();
             $table->string('photo_path')->nullable();
-            $table->string('role')->default('barberman');
+            $table->string('role')->default('karyawan');
             $table->rememberToken();
             $table->timestamps();
         });
